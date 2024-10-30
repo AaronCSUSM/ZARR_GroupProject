8509b2b9c72596e31af65e35e4fd490dcf6e0a1f
Aaron Cambridge <cambr005@csusm.edu>
2024-10-30 14:29:23 -0700

    added Junit test cases for findCity and getCoordinates functions

--- src/zarr/JUnitTest.java
+++ src/zarr/JUnitTest.java
@@ -2,6 +2,11 @@ package zarr;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -27,8 +32,98 @@ class JUnitTest {
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	void testFindCity() {
+		DatabaseClient db = new DatabaseClient();
+		
+		boolean expected1 = true;
+		boolean expected2 = true;
+		boolean expected3 = false;
+		boolean expected4 = false;
+		
+		boolean result1 = false;
+		try {
+			result1 = db.findCity("Tokyo", "Japan", null);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		boolean result2 = false;
+		try {
+			result2 = db.findCity("New York City", "USA", "New York");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		boolean result3 = true;
+		try {
+			result3 = db.findCity("Tokyo", "USA", null);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		boolean result4 = true;
+		try {
+			result4 = db.findCity("New York City", "USA", null);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		assertEquals(expected1, result1, "City should be in database.");
+		assertEquals(expected2, result2, "City should be in database.");
+		assertEquals(expected3, result3, "City with those properties doesn't exist.");
+		assertEquals(expected4, result4, "City with those properties doesn't exist.");
+		
+		System.out.println("End of test 1");
+		
+	}//end findCity test
+	
+	@Test
+	void testGetCoordinates() {
+		DatabaseClient db = new DatabaseClient();
+		
+		double[] expected1 = {35.67, 139.65};
+		double[] expected2 = {40.71, -74.00};
+		double[] expected3 = {59.93, 30.36};	
+		double[] expected4 = {30.04, 31.23};	
+		
+		double[] result1 = new double[2];
+		double[] result2 = new double[2];
+		double[] result3 = new double[2];
+		double[] result4 = new double[2];
+		
+		try {
+			result1 = db.getCoordinates("Tokyo", "Japan", null);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			result2 = db.getCoordinates("New York City", "USA", "New York");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			result3 = db.getCoordinates("St. Petersburg", "Russia", null);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			result4 = db.getCoordinates("Cairo", "Egypt", null);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		assertArrayEquals(expected1, result1, "Incorrect coordinates for test1.");
+		assertArrayEquals(expected2, result2, "Incorrect coordinates for test2.");
+		assertArrayEquals(expected3, result3, "Incorrect coordinates for test3.");
+		assertArrayEquals(expected4, result4, "Incorrect coordinates for test4.");
+		
+		System.out.println("end test 2");
+		
+	}//end testGetCoordinates
 
 }
